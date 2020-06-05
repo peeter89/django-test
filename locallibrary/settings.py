d61b9994437030093dec151015fa5f76b3dbfa70
peeter89 <38555164+peeter89@users.noreply.github.com>
2020-06-05 20:20:39 +0200

    update settings.py
    delete was config

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -145,17 +145,3 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-
-if (DEBUG == False):
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-
-    AWS_LOCATION = 'static'
-
-    DEFAULT_FILE_STORAGE = 'locallibrary.storage_backends.MediaStorage'
